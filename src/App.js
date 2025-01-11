0b3186ead209d54c0ab1442889e14a9b58ae70c3
yaroslav <neselivanovaroslav@gmail.com>
2025-01-11 16:01:04 +0300

    Create Card product component. Crate first props for Card component and testing this in html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+// const productArr = [
+//     {productTitle: "Мужские Кроссовки Nike Air Max 270", priceProduct: "12 990 руб."},
+//     {productTitle: "Мужские Кроссовки Nike Blazer Mid Suede", priceProduct: "10 500 руб."},
+//     {productTitle: "Кроссовки Puma X Aka Boku Future Rider", priceProduct: "8 999 руб."},
+//     {productTitle: "Кроссовки Puma X Aka Boku Future Rider", priceProduct: "8 999 руб."},
+// ]
+
 export default function App() {
     return (
         <div className="wrapper clear">
@@ -20,10 +27,16 @@ export default function App() {
                     </div>
                 </div>
                 <div className="sneakers d-flex flex-wrap">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    <Card
+                        title="Мужские Кроссовки Nike Air Max 270"
+                        price={14560}
+                        imageURL="/images/jpg/nike-air-max.jpg"
+                    />
+                    <Card
+                        title="Мужские Кроссовки Nike Air Max 270"
+                        price={14560}
+                        imageURL="/images/jpg/nike-air-max.jpg"
+                    />
                 </div>
             </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
-export default function DefaultCardProduct() {
+export default function Card({ title, imageURL, price }) {
     return (
         <div className="card mt-40">
             <div className="favorite">
                 <img src="/images/svg/heart-default-button.svg" alt="heart button"/>
             </div>
-            <img width={132} height={112} src="/images/jpg/nike-air-max.jpg"
-                 alt="Мужские Кроссовки Nike Air Max 270"/>
-            <p>Мужские Кроссовки Nike Air Max 270</p>
+            <img width={132} height={112} src={imageURL}
+                 alt={title}/>
+            <p>{title}</p>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span className="price">Цена:</span>
-                    <b>12 990 руб.</b>
+                    <b>{price} руб.</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/images/svg/plus.svg" alt=""/>
